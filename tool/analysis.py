4f8231274f77fb24e96e35b10cd39d6e2719fbaf
Roo Case <roo@roocase.org>
2024-11-09 21:02:18 -0600

    analysis averages

--- tool/analysis.py
+++ tool/analysis.py
@@ -1,7 +1,7 @@
 import time
 import subprocess
 from memory_profiler import memory_usage
-def run_program(type):
+def run_program(type, OUTPUTruntime):
     commands = [
         ["hydra", "-L", "usernames.txt", "-P", "passwords.txt", "18.188.93.218", "http-get", "/important/SECRETCODES.html"],
         ["./raspray", "-u", "usernames.txt", "-p", "passwords.txt", "-i", "18.188.93.218"]
@@ -14,28 +14,61 @@ def run_program(type):
     runtime = end_time - start_time
     
     # Print command output
-    print("Results from running", commands[type][0], ":")
+    """print("Results from running", commands[type][0], ":")
     print("Output:", stdout.decode())
     print("Error Output:", stderr.decode())
     print(f"Runtime of {commands[type][0]} command: {runtime:.2f} seconds")
+    """
+    OUTPUTruntime = runtime
 
 # Measure memory usage
 def profile_memory_usage(num):
     """Profiles memory usage of the Hydra command."""
-    mem_usage = memory_usage((run_program, (num,)), max_iterations=10, interval=0.05)
+    runtime = 0
+    mem_usage = memory_usage((run_program, (num, runtime)), max_iterations=10, interval=0.05)
     peak_memory = max(mem_usage)
-    print(f"Peak Memory Usage: {peak_memory:.2f} MB")
+    """print(f"Peak Memory Usage: {peak_memory:.2f} MB")"""
+
+    return runtime, peak_memory
 
 # Execute the memory profiling and runtime measurement
 if __name__ == "__main__":
-    print("Running hydra command...")
-    profile_memory_usage(0)
+    totalRuns = 15
+    tottimeHYDRA = 0
+    tottimeRASP = 0
+    totmemHYDRA = 0
+    totmemRASP = 0
+
+    for i in range(totalRuns):
+        print("Running hydra command...")
+        runtimeHYDRA, memHYDRA = profile_memory_usage(0)
+        tottimeHYDRA += runtimeHYDRA
+        totmemHYDRA += memHYDRA
+
+        print("\n")
+        print("Waiting for 10 seconds before running the next command to let server cool down...")
+        time.sleep(10)
+
+        print("\n") 
+        print("Running raspray command...")
+        runtimeRASP, memRASP = profile_memory_usage(1)
+        tottimeRASP += runtimeRASP
+        totmemRASP += memRASP
+
+        print("\n")
+        print("Waiting for 30 seconds before running the next run to let server cool down...")
+        time.sleep(30)
+
+    avgHYDRA = tottimeHYDRA / totalRuns
+    avgRASP = tottimeRASP / totalRuns
+    avgmemHYDRA = totmemHYDRA / totalRuns
+    avgmemRASP = totmemRASP / totalRuns
 
     print("\n")
-    print("Waiting for 10 seconds before running the next command to let server cool down...")
-    time.sleep(10)
+    print("Average runtime for Hydra command:", avgHYDRA)
+    print("Average runtime for Raspray command:", avgRASP)
+    print("Average memory usage for Hydra command:", avgmemHYDRA)
+    print("Average memory usage for Raspray command:", avgmemRASP)
 
-    print("\n") 
-    print("Running raspray command...")
-    profile_memory_usage(1)
     
+        
