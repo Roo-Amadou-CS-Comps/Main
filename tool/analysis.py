228e2dd3d2bea70304dc467f4f53006d63a1f2d0
Roo Case <roo@roocase.org>
2024-11-10 15:55:33 -0600

    results

--- tool/analysis.py
+++ tool/analysis.py
@@ -35,8 +35,33 @@ def profile_memory_usage(num):
     return runtime[0], peak_memory
 
 # Execute the memory profiling and runtime measurement
+
+def write_results(totalRuns, avgHYDRA, avgRASP, avgmemHYDRA, avgmemRASP, hydratimes, rasptimes, hydratimesOutliers, rasptimesOutliers, memHYDRAs, memRASPs):
+
+    # Write the results to a file
+    with open("results.txt", "w") as f:
+        f.write(f"Attempted to run the commands {totalRuns} times each.\n")
+        f.write("Results of the analysis:\n\n")
+        f.write(f"Average runtime for Hydra command: {avgHYDRA}\n")
+        f.write(f"Average runtime for Raspray command: {avgRASP}\n")
+        f.write(f"Average memory usage for Hydra command: {avgmemHYDRA}\n")
+        f.write(f"Average memory usage for Raspray command: {avgmemRASP}\n")
+        f.write("\n")
+        f.write(f"All runtimes for Hydra command: {hydratimes}\n")
+        f.write(f"All runtimes for Raspray command: {rasptimes}\n")
+        f.write("\n")
+        f.write(f"Outliers for Hydra command: {hydratimesOutliers}\n")
+        f.write(f"Outliers for Raspray command: {rasptimesOutliers}\n")
+        f.write("\n")
+        f.write("Memory usage for Hydra command:\n")
+        f.write(str(memHYDRAs))
+        f.write("\n")
+        f.write("Memory usage for Raspray command:\n")
+        f.write(str(memRASPs))
+
+
 if __name__ == "__main__":
-    totalRuns = 20
+    totalRuns = 500
     tottimeHYDRA = 0
     tottimeRASP = 0
     totmemHYDRA = 0
@@ -50,10 +75,14 @@ if __name__ == "__main__":
     rasptimesOutliers = []
     hydratimesOutliers = []
 
+    memHYDRAs = []
+    memRASPs = []
+
     for i in range(1, totalRuns+1):
         print(f"\n\nRound {i}:\n\nRunning hydra command...")
         runtimeHYDRA, memHYDRA = profile_memory_usage(0)
         hydratimes.append(runtimeHYDRA)
+        memHYDRAs.append(memHYDRA)
         tottimeHYDRA += runtimeHYDRA
         totmemHYDRA += memHYDRA
 
@@ -63,6 +92,7 @@ if __name__ == "__main__":
         print("Running raspray command...")
         runtimeRASP, memRASP = profile_memory_usage(1)
         rasptimes.append(runtimeRASP)
+        memRASPs.append(memRASP)
         tottimeRASP += runtimeRASP
         totmemRASP += memRASP
 
@@ -77,14 +107,25 @@ if __name__ == "__main__":
             rasptimesOutliers.append(runtimeRASP)
 
         if i < totalRuns - 1:
-            print("Waiting for 10 seconds before running the next run to let server cool down...")
-            time.sleep(10)
+            if i % 10 == 0:
+                print("Writing results to file...")
+                write_results(i, tottimeHYDRA / i, tottimeRASP / i, totmemHYDRA / i, totmemRASP / i, hydratimes, rasptimes, hydratimesOutliers, rasptimesOutliers, memHYDRAs, memRASPs)
+            if  i % 100 == 0:
+                print("Waiting for 45 seconds before running the next run to let server cool down...")
+                time.sleep(45)
+            else:
+                print("Waiting for 10 seconds before running the next run to let server cool down...")
+                time.sleep(10)
+        
 
     avgHYDRA = tottimeHYDRA / totalRuns
     avgRASP = tottimeRASP / totalRuns
     avgmemHYDRA = totmemHYDRA / totalRuns
     avgmemRASP = totmemRASP / totalRuns
 
+    print("\n")
+    print(f"Attempted to run the commands {totalRuns} times each.")
+    print("Results of the analysis:")
     print("\n")
     print("Average runtime for Hydra command:", avgHYDRA)
     print("Average runtime for Raspray command:", avgRASP)
@@ -96,17 +137,18 @@ if __name__ == "__main__":
     print("\n")
     print("All runtimes for Hydra command without outliers:", hydratimesWithoutOutliers)
     print("All runtimes for Raspray command without outliers:", rasptimesWithoutOutliers)
+    print("\n")
+    print("Outliers for Hydra command:", hydratimesOutliers)
+    print("Outliers for Raspray command:", rasptimesOutliers)
+    print("\n")
+    print("Memory usage for Hydra command:")
+    print(memHYDRAs)
+    print("\n")
+    print("Memory usage for Raspray command:")
+    print(memRASPs)
 
-    # Write the results to a file
-    with open("results.txt", "w") as f:
-        f.write(f"Average runtime for Hydra command: {avgHYDRA}\n")
-        f.write(f"Average runtime for Raspray command: {avgRASP}\n")
-        f.write(f"Average memory usage for Hydra command: {avgmemHYDRA}\n")
-        f.write(f"Average memory usage for Raspray command: {avgmemRASP}\n")
-        f.write(f"All runtimes for Hydra command: {hydratimes}\n")
-        f.write(f"All runtimes for Raspray command: {rasptimes}\n")
-        f.write(f"All runtimes for Hydra command without outliers: {hydratimesWithoutOutliers}\n")
-        f.write(f"All runtimes for Raspray command without outliers: {rasptimesWithoutOutliers}\n")
+    write_results(totalRuns, avgHYDRA, avgRASP, avgmemHYDRA, avgmemRASP, hydratimes, rasptimes, hydratimesOutliers, rasptimesOutliers, memHYDRAs, memRASPs)
+
 
     
         
