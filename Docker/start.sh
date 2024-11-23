3e699bff85d0983ffecb7bc620e77127a9f35553
Roo Case <roo@roocase.org>
2024-11-22 20:55:19 -0600

    start.sh adjustment

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Install Composer dependencies
+echo "Checking for Composer..."
+if ! command -v composer &> /dev/null; then
+    echo "Composer not found. Installing Composer..."
+    apt-get update && apt-get install -y wget php-cli unzip
+    wget -O composer-setup.php https://getcomposer.org/installer
+    php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+fi
+
+echo "Installing PHP dependencies..."
+cd /var/www/html/loginProtect || exit 1
+composer require whitehat101/apr1-md5
+if [ $? -ne 0 ]; then
+    echo "Failed to install Composer dependencies. Exiting."
+    exit 1
+fi
+
+cd /var/www/html/evenmoreimportant || exit 1
+composer require whitehat101/apr1-md5
+if [ $? -ne 0 ]; then
+    echo "Failed to install Composer dependencies. Exiting."
+    exit 1
+fi
+
+
 # Start Apache
 echo "Starting Apache..."
 service apache2 restart
@@ -7,8 +32,6 @@ if [ $? -ne 0 ]; then
     echo "Failed to start Apache. Exiting."
     exit 1
 fi
-
-
 
 # Start Nginx
 echo "Starting Nginx..."
@@ -18,5 +41,12 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+
+# Set permissions for the vendor directory
+echo "Setting permissions for the vendor directory..."
+chmod -R 755 /var/www/html/loginProtect/vendor
+
 # Keep the container running
+echo "Startup complete. Tailing logs..."
 tail -f /var/log/apache2/access.log /var/log/nginx/access.log
+
