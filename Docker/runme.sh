334563c1a28eaf85dc462aee712dd17ce7405fde
Roo Case <roo@roocase.org>
2024-11-22 23:31:09 -0600

    Docker changes

--- Docker/runme.sh
+++ Docker/runme.sh
@@ -1,1 +1,1 @@
-docker build --no-cache -t target_server . && docker run -it -p 80:80 -p 8080:8080 target_server /bin/bash
+docker build --no-cache -t target_server . && docker run -it -p 80:80 target_server
